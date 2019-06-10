fa201897b9d0a560257a0845cdde3b161dec3cc5
Chris Windsor <mrchriswindsor@gmail.com>
2019-06-10 17:06:20 -0400

    Removed unnecessary render code

--- src/vdom/Render.ts
+++ src/vdom/Render.ts
@@ -3,9 +3,8 @@ import { vNode } from "../globals";
 import computeEvent from "../attributes/Events";
 import { getProp } from "../data/DataFns";
 import computeBinding from "../attributes/Bindval";
-import createEl from "./CreateElement";
 
-export const renderElem = (instance: Polarbear, {tagName, attrs = {}, events = {}, conditionalCase, loopCase, boundData, refName, children = []}: vNode): any => {
+export const renderElem = (instance: Polarbear, {tagName, attrs = {}, events = {}, conditionalCase, boundData, refName, children = []}: vNode): any => {
   // Evaluate conditional statement for the element
   const conditionalEval: boolean = Boolean(Function(`"use strict";return ${conditionalCase};`)
     .call(instance));
@@ -29,45 +28,10 @@ export const renderElem = (instance: Polarbear, {tagName, attrs = {}, events = {
     $el.addEventListener(eventName, fn, otherEventModifiers);
   }
 
-  loopCase = loopCase ? parseInt(loopCase as string) : null;
-
-  const loopCaseRenderedChildren: any = [];
-
   // Render and append element children
   for (const child of children) {
     const $child = render(instance, child);
-
-    if (loopCase !== null) {
-      loopCaseRenderedChildren.push(child);
-    } else {
-      if (Array.isArray($child)) {
-        $child.forEach(el => {
-          $el.appendChild(renderElem(instance, el) as HTMLElement);
-        });
-      } else {
-        $el.appendChild($child);
-      }
-    }
-  }
-
-  if (loopCase) {
-    // @ts-ignore
-    console.log("Shouldn't be coming here", arguments[1]);
-
-    return Array.from(new Array(loopCase), (v, i) => {
-      return createEl(tagName, {
-        attrs,
-        events,
-        conditionalCase,
-        loopCase: null,
-        boundData,
-        refName: null,
-        children: [
-          "hello" + i,
-          ...loopCaseRenderedChildren
-        ]
-      });
-    });
+    $el.appendChild($child);
   }
 
   if (boundData && $el instanceof HTMLInputElement) {
